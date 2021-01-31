1ab2a12b22cbc56cbff4369e2945c56d1a4e0e24
Sardar <skhan@stellatechnology.com>
2021-01-31 10:11:32 +0500

    Add Scopping . Activity , fragment and Singleton scope example.

--- app/src/main/java/com/example/hiltexample/MainActivity.kt
+++ app/src/main/java/com/example/hiltexample/MainActivity.kt
@@ -3,8 +3,12 @@ package com.example.hiltexample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.fragment.app.Fragment
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.scopes.ActivityScoped
+import dagger.hilt.android.scopes.FragmentScoped
 import javax.inject.Inject
+import javax.inject.Singleton
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -12,14 +16,25 @@ class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var myClasss : SomeClass
 
+    @Inject
+    lateinit var myActivity: myActivity
+
+    // this dependency injection won't works because its a lower order dependency than activity.
+    // Fragment scope is less than activity scope.
+    // Lower scope cannot be injected in to higher scope in tier system.
+//    @Inject
+//    lateinit var myFragment1: myFragment1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d(MainActivity::class.simpleName, "onCreate: ${myClasss.doSomething()}")
         Log.d(MainActivity::class.simpleName, "onCreate: ${myClasss.doSomeOtherThing()} ")
+        Log.d(MainActivity::class.simpleName, "onCreate: ${myActivity.doSomethingOther()}")
     }
 }
 
+// Constructor Injection...
 // @Inject make it available at compile time.
 class SomeClass @Inject constructor(private val someOtherClass: SomeOtherClass) {
 
@@ -32,8 +47,31 @@ class SomeClass @Inject constructor(private val someOtherClass: SomeOtherClass) 
 }
 
 // First Dagger Build this instance and pass that instance to the SomeClass and then create instance of someClass and inject it to mainActivity .
+@Singleton // make this dependency live as long as application is alive..
 class SomeOtherClass @Inject constructor() {
     fun doSomethingOther():String{
         return "sardar khan"
     }
 }
+
+@ActivityScoped // make this dependency live as long as Activity is alive..
+class myActivity @Inject constructor() {
+    fun doSomethingOther():String{
+        return "My Activity is running"
+    }
+}
+
+@FragmentScoped // make this dependency live as long as Fragment is alive..
+class myFragment1 @Inject constructor() {
+    fun doSomethingOther():String{
+        return "My Activity is running"
+    }
+}
+
+@AndroidEntryPoint
+class myFragment: Fragment(){
+    // Field Injection
+    @Inject lateinit var someClass: SomeClass
+
+}
+
